0a9f3c7b179cd2ff6f8b3ac585237591bbc2cbd4
shubham <49603826+shubhdevelop@users.noreply.github.com>
2024-08-13 19:23:11 +0530

    [breaking change] introduced signaling server

--- src/Components/Send.tsx
+++ src/Components/Send.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useMemo, useState } from 'react'
-
+import { nanoid } from 'nanoid';
+import  {io}  from 'socket.io-client';
 const configuration = {
     iceServers: [
         {
@@ -8,91 +9,86 @@ const configuration = {
     ]
 };
 
+
 function Send() {
-    const [offer, setOffer] = useState('')
-    const [message, setMessage] = useState('')
-    const [messageList, setMessageList] = useState<string[] | []>([])
-    const inputRef = useRef<HTMLTextAreaElement | null>(null)
+
+    const socket = io('https://signaling-serve.onrender.com', {transports:['websocket']})
     const lc = useRef(new RTCPeerConnection(configuration)).current
     const dc = useMemo(() => lc.createDataChannel('channel'), [lc])
+    const [message, setMessage] = useState<string>('')
+    const [messageList, setMessageList] = useState<string[]>([]);
 
-    function handleAnswer() {
-        if (inputRef.current) {
-            const answer = JSON.parse(inputRef.current.value)
-            lc.setRemoteDescription(answer)
-        }
+    function handlesSendMessage (){
+        dc.send(message);
+        setMessageList(prev => [...prev, message]);
+        setMessage('');
     }
 
     useEffect(() => {
-        lc.onicecandidate = () => {
+    
+    function onConnect(){
+        console.log('connected');
+        
+    }
+    function onRecieveAnswer (answer:any){
+            console.log(answer, ":recieved!!");
+            lc.setRemoteDescription(answer);
+            console.log("remote Description set!!");
+    }
+
+    function onDisconnect(){
+        console.log("disconnected");
+        
+    }
+
+    function onIceCandidate() {
             console.log(
-                'found new Ice Candidate:',
+                'found new Ice Candidate:', 
                 JSON.stringify(lc.localDescription)
             )
-            setOffer(JSON.stringify(lc.localDescription))
-        }
+            socket.emit('offer-forward', lc.localDescription)
+            console.log("ice candidiate send!!");
+            
+    }
 
-        lc.createOffer().then((e) => {
-            console.log('creating an offer')
+
+
+    socket.on('connect', onConnect)
+    socket.on("recieve-answer",onRecieveAnswer);
+    socket.on('disconnect',onDisconnect);
+    lc.onicecandidate = onIceCandidate;
+    lc.createOffer().then((e) => {
            lc.setLocalDescription(e)
-        })
-    }, [lc])
+    })
+
+    return ()=>{
+    socket.off('connect', onConnect)
+    socket.off("recieve-answer",onRecieveAnswer);
+    socket.off('disconnect',onDisconnect)
+    }
+
+    }, [lc, socket])
 
     useEffect(() => {
         dc.onopen = () => console.log('Connection Opened!!')
         dc.onmessage = (e) => {
             console.log('messsage received!!!' + e.data)
-            setMessageList((prev) => [...prev, `sender: ${e.data}`])
         }
         dc.onclose = () => console.log('close')
     }, [dc])
 
     return (
         <div>
-            <code className=" block text-black whitepace-pre bg-white w-[60%] overflow-x-scroll p-2 m-2 border-[1px] shadow-md rounded-md">
-                {offer}
-            </code>
-            <textarea
-                className="px-3 py-2 m-2 border-[1px] shadow-md rounded-sm w-96"
-                placeholder="Set your ice here!!"
-                ref={inputRef}
-            />
-            <button
-                className="bg-black text-white border-[1px] rounded-md px-4 py-2 cursor-pointer"
-                onClick={handleAnswer}
-            >
-                {' '}
-                Set here
-            </button>
+        <div className='p-4 w-full'>
+        <h1>Message</h1>
+        {
+            messageList.map(message=><p key={nanoid()} className='py-2 text-blue-500 px-3 w-full text-left border-[1px]'>{message}</p>)
 
-            <code className=" block text-black whitepace-pre bg-white w-[60%] overflow-x-scroll p-2 m-2 border-[1px] shadow-md rounded-md"></code>
-            <h1> Message </h1>
-            <input
-                value={message}
-                className="border-[1px]"
-                type="text"
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <button
-                onClick={() => {
-                    dc.send(message)
-                    setMessageList((prev) => [...prev, `shubham: ${message}`])
-                    setMessage('')
-                }}
-                className="border-[1px] py-2 px-3 bg-black shadow-md rounded-md text-white m-3"
-            >
-                {' '}
-                Send{' '}
-            </button>
+        }
+        <input type='text' value={message} onChange={(e)=>{setMessage(e.target.value)}} className='py-2 px-3 border-[1px] shadow-md mr-2' />
+        <button onClick={handlesSendMessage} className='px-3 py-2 bg-black text-white rounded-md border-[1px] shadow-md'> Send </button>
 
-            <div className="w-[50%] p-4 bg-white text-black shadow-md rounded-sm">
-                {messageList.map((message) => (
-                    <p className="border-[1px] p-4 text-blue-400">
-                        {' '}
-                        {message}{' '}
-                    </p>
-                ))}
-            </div>
+        </div>
         </div>
     )
 }
